f912b77f2d1b9c9cc8f4d391f3c9cf3672c8cce8
Caio Barroso <caio.barroso@data-techsolution-brazil.com>
2024-12-17 16:46:55 -0300

    Mensagem do commit descrevendo as mudanças

--- WEB.SCRAPING.py
+++ WEB.SCRAPING.py
@@ -69,7 +69,7 @@ st.markdown("<div style='margin-top: 50px;'></div>", unsafe_allow_html=True)
 # ---------------------------------------- #
 
 # Adicionando imagem no topo da barra lateral
-st.sidebar.image("images/infulltech.jpg", use_column_width=True)
+st.sidebar.image("images/infulltech.jpg", use_container_width=True)
 st.sidebar.markdown("<h1 style='text-align: center; width: 100%;'>Infull Tech</h1>", unsafe_allow_html=True)
 
 # Botão para carregar o arquivo Excel
