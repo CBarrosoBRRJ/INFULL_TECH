5fa9356f4de1dde0c1660c299d2a41a646e7d58b
Caio Barroso <caio.barroso@data-techsolution-brazil.com>
2024-12-17 16:47:59 -0300

    Mensagem do commit descrevendo as mudanças

--- WEB.SCRAPING.py
+++ WEB.SCRAPING.py
@@ -69,7 +69,10 @@ st.markdown("<div style='margin-top: 50px;'></div>", unsafe_allow_html=True)
 # ---------------------------------------- #
 
 # Adicionando imagem no topo da barra lateral
-st.sidebar.image("images/infulltech.jpg", use_container_width=True)
+if not os.path.exists("images/infulltech.jpg"):
+    st.sidebar.error("Erro: Imagem não encontrada no caminho especificado.")
+else:
+    st.sidebar.image("images/infulltech.jpg", use_container_width=True)
 st.sidebar.markdown("<h1 style='text-align: center; width: 100%;'>Infull Tech</h1>", unsafe_allow_html=True)
 
 # Botão para carregar o arquivo Excel
